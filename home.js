7b664ef76514d6e0946a403c24f1d7546d503044
WebTechFlore <WebTechFore@gmail.com>
2025-06-25 22:38:35 +0200

    Add files via upload

---
+++ home.js
@@ -0,0 +1,13 @@
+function toggleMode() {
+  document.body.classList.toggle('dark');
+}
+// Funktion zum Umschalten der Antworten im FAQ
+function toggleAnswer(answerId) {
+  var answerElement = document.getElementById(answerId);
+  // Toggle die Anzeige der Antwort
+  if (answerElement.style.display === "block") {
+    answerElement.style.display = "none";
+  } else {
+    answerElement.style.display = "block";
+  }
+}
